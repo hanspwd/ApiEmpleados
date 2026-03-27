21bb739ffb6f1507eb8674d8a168cf6053a3e5d3
Hans <hwnss.w@gmail.com>
2026-03-26 23:39:30 -0300

    basic api

---
+++ src/main/java/com/hans/apiempleados/ApiEmpleadosApplication.java
@@ -0,0 +1,12 @@
+package com.hans.apiempleados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEmpleadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEmpleadosApplication.class, args);
+	}
+}
