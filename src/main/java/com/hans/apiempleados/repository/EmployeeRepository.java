4b06b21ce3911dfd90127a615d3f07636d35a0de
Hans <hwnss.w@gmail.com>
2026-03-27 16:40:41 -0300

    Mini fix update

--- src/main/java/com/hans/apiempleados/repository/EmployeeRepository.java
+++ src/main/java/com/hans/apiempleados/repository/EmployeeRepository.java
@@ -3,16 +3,15 @@ package com.hans.apiempleados.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.hans.apiempleados.model.Employee;
 
-import lombok.Data;
-
 @Repository
-@Data
 public class EmployeeRepository {
     
+    @Autowired
     private List<Employee> empleados;
 
     public EmployeeRepository() {
