54b54a954d74b59d5f657f57e6de66e7fb359684
Hans <hwnss.w@gmail.com>
2026-03-26 23:38:19 -0300

    Mini feat: uncompleted EmployeeController implementation

--- src/main/java/com/hans/apiempleados/ApiEmployeeApplication.java
+++ src/main/java/com/hans/apiempleados/ApiEmployeeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiEmpleadosApplication {
+public class ApiEmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiEmpleadosApplication.class, args);
+		SpringApplication.run(ApiEmployeeApplication.class, args);
 	}
 }
---
+++ src/main/java/com/hans/apiempleados/controller/EmployeeController.java
@@ -0,0 +1,34 @@
+package com.hans.apiempleados.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hans.apiempleados.model.Employee;
+import com.hans.apiempleados.service.EmployeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    
+    @Autowired
+    private EmployeService employeService;
+
+    @GetMapping()
+    public List<Employee> getEmployees() {
+        return employeService.getAllEmployees();
+    }
+
+    @PostMapping()
+    public Employee postMethodName(@RequestBody Employee employee) {   
+        return employeService.saveEmployee(employee);
+    }
+    
+    
+
+}
